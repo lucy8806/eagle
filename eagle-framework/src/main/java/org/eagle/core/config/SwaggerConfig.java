29070baa420a84aa8d34fc8fcdbcc31ba0470a84
lucy <lucy@PC-201706142256>
2019-05-05 22:22:53 +0800

    添加单点登录工程

--- eagle-framework/src/main/java/org/eagle/core/config/SwaggerConfig.java
+++ eagle-framework/src/main/java/org/eagle/core/config/SwaggerConfig.java
@@ -1,12 +1,18 @@
 package org.eagle.core.config;
 
+import static com.google.common.collect.Lists.newArrayList;
+
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.annotations.ApiOperation;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -14,7 +20,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @Description:API文档生成配置
- * @author Administrator
+ * @author lucy
  * @date:2019年5月4日 下午8:23:55
  *
  */
@@ -24,16 +30,29 @@ public class SwaggerConfig {
 
 	@Bean
 	public Docket api() {
+		/*
+		 * return new
+		 * Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select() //
+		 * 包路径 .apis(RequestHandlerSelectors.basePackage(
+		 * "org.eagel.admin.sys.controller")).paths(PathSelectors.any())
+		 * .build();
+		 */
 		return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
-				// 包路径
-				.apis(RequestHandlerSelectors.basePackage("org.eagel.admin.sys.controller")).paths(PathSelectors.any())
-				.build();
+				// 加了ApiOperation注解的类，才生成接口文档
+				.apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+				// 包下的类，才生成接口文档
+				// .apis(RequestHandlerSelectors.basePackage("org.eagel.admin.sys.controller"))
+				.paths(PathSelectors.any()).build().securitySchemes(security());
 	}
 
 	private ApiInfo apiInfo() {
-		return new ApiInfoBuilder().title("hope-admin权限管理系统API操作文档").description("后台管理中心 API 1.0 操作文档")
+		return new ApiInfoBuilder().title("eagle-admin权限管理系统API操作文档").description("后台管理中心 API 1.0 操作文档")
 				// 服务条款网址
 				.termsOfServiceUrl("https://aodeng.cc").version("2.0")
 				.contact(new Contact("Lucy", "https://aodeng.cc", "java@aodeng.cc")).build();
+	}
+
+	private List<ApiKey> security() {
+		return newArrayList(new ApiKey("token", "token", "header"));
 	}
 }
