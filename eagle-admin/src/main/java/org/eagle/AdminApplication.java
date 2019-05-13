69003d82d61295c14ae44c2902d2d10dd062932f
lusai <hx0012031@hengxinyongli.com>
2019-05-13 18:06:33 +0800

    跨越访问配置

--- eagle-admin/src/main/java/org/eagle/AdminApplication.java
+++ eagle-admin/src/main/java/org/eagle/AdminApplication.java
@@ -2,6 +2,9 @@ package org.eagle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AdminApplication {
@@ -10,4 +13,18 @@ public class AdminApplication {
 		SpringApplication.run(AdminApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("*")
+						.allowedHeaders("*")
+						.allowCredentials(true)
+						.maxAge(3600);
+			}
+		};
+	}
 }
